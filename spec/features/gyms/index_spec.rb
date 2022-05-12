2f77bc25bc8e8c58b97cd872ea9f263888e0fc95
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-12 13:12:12 -0600

    test: gyms#index

---
+++ spec/features/gyms/index_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "gyms#index", type: :feature do
+
+  # User Story 1, Parent Index
+  #
+  # For each parent table
+  # As a visitor
+  # When I visit '/parents'
+  # Then I see the name of each parent record in the system
+  it "displays names of gyms" do
+    
+  end
+end
