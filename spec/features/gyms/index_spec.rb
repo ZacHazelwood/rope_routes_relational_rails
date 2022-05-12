00b8f099c90e1e05b90ba438d72918dcf8efdc5d
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-12 13:23:00 -0600

    test: User Story 1

--- spec/features/gyms/index_spec.rb
+++ spec/features/gyms/index_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe "gyms#index", type: :feature do
   # When I visit '/parents'
   # Then I see the name of each parent record in the system
   it "displays names of gyms" do
-    
+    gym_1 = Gym.create(name: "Movement Englewood", location: "Englewood, CO", has_rope: true, square_feet: 175000)
+    gym_2 = Gym.create(name: "Movement Boulder", location: "Boulder, CO", has_rope: true, square_feet: 22000)
+    visit "/gyms"
+
+    expect(page).to have_content(gym_1.name)
+    expect(page).to have_content(gym_2.name)
   end
 end
