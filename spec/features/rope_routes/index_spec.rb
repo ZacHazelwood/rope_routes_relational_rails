4d8aca01cb1f3f59ef6fba5f05943f28cca7e319
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-12 14:47:22 -0600

    test:User Story 3

--- spec/features/rope_routes/index_spec.rb
+++ spec/features/rope_routes/index_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'rope_routes#index', type: :feature do
 
+  # User Story 3, Child Index
+  #
+  # As a visitor
+  # When I visit '/child_table_name'
+  # Then I see each Child in the system including the Child's attributes:
+  it "displays rope routes and their attributes" do
 
-
-end  
+  end
+end
