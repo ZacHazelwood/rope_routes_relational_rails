a4dfc00dbf16746780cbef12e7ba7a696e8fd36b
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-13 15:14:11 -0600

    test: parent-child association

--- spec/features/gyms/rope_routes/index_spec.rb
+++ spec/features/gyms/rope_routes/index_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe 'rope_routes#show', type: :feature do
+RSpec.describe "Parent-Child index", type: :feature do
 
-  # User Story 4, Child Show
+  # User Story 5, Parent Children Index
   #
   # As a visitor
-  # When I visit '/child_table_name/:id'
-  # Then I see the child with that id including the child's attributes:
-  it 'displays a rope route from an ID and displays attributes' do
+  # When I visit '/parents/:parent_id/child_table_name'
+  # Then I see each Child that is associated with that Parent with each Child's attributes:
+  it "associates the child table to the parent ID" do
     gym_1 = Gym.create!(name: "Movement Englewood", location: "Englewood, CO", has_rope: true, square_feet: 175000)
     gym_2 = Gym.create!(name: "Movement Boulder", location: "Boulder, CO", has_rope: true, square_feet: 22000)
     rope_1 = gym_1.rope_routes.create!(grade: '5.9', color: 'Green', top_rope: true, lead: false, height: 33)
@@ -15,7 +15,7 @@ RSpec.describe 'rope_routes#show', type: :feature do
     rope_3 = gym_2.rope_routes.create!(grade: '5.10a', color: 'White', top_rope: true, lead: true, height: 33)
     rope_4 = gym_2.rope_routes.create!(grade: '5.11', color: 'White', top_rope: true, lead: true, height: 38)
 
-    visit "/rope_routes/#{rope_1.id}"
+    visit "/gym/#{gym_1.id}/rope_routes"
     # save_and_open_page
 
     expect(page).to have_content(rope_1.id)
