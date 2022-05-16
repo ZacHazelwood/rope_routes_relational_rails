162bed9d6bf14ee9e11f534608469f1a2e2eb16f
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-16 13:17:53 -0600

    test:US11, gym#create

---
+++ spec/features/gyms/new_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "New Gym Creation", type: :feature do
+  describe "User Story 11" do
+    # User Story 11, Parent Creation
+
+    # As a visitor
+    # When I visit the Parent Index page
+    # Then I see a link to create a new Parent record, "New Parent"
+    # When I click this link
+    # Then I am taken to '/parents/new' where I  see a form for a new parent record
+    # When I fill out the form with a new parent's attributes:
+    # And I click the button "Create Parent" to submit the form
+    # Then a `POST` request is sent to the '/parents' route,
+    # a new parent record is created,
+    # and I am redirected to the Parent Index page where I see the new Parent displayed.
+    it "can create a Gym using a form" do
+        visit "/gyms/new"
+
+        fill_in(:name, with: "Movement RiNo")
+        fill_in(:location, with: "Denver, CO")
+        fill_in(:has_rope, with: false)
+
+        click_button("Create Gym")
+
+        expect(current_path).to eq("/gym")
+        expect(page).to have_content("Movement RiNo")
+        expect(page).to_not have_content("The Spot")
+    end
+  end
+end
