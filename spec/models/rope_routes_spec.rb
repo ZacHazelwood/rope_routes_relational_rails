a9f2153607e579e32f85403ce59ad4254fce68d6
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-16 12:13:04 -0600

    Validate model associations

--- spec/models/rope_routes_spec.rb
+++ spec/models/rope_routes_spec.rb
@@ -4,18 +4,18 @@ RSpec.describe RopeRoute, type: :model do
   describe "Table and relationship validation" do
     describe "relationship" do
 
-      xit { should belong_to :gym }
+      it { should belong_to :gym }
     end
 
     describe "Table validations" do
 
-      xit { should validate_presence_of :grade }
-      xit { should validate_presence_of :color }
-      xit { should validate_presence_of :height }
-      xit { should allow_value(true).for(:top_rope) }
-      xit { should allow_value(false).for(:top_rope) }
-      xit { should allow_value(true).for(:lead) }
-      xit { should allow_value(false).for(:lead) }
+      it { should validate_presence_of :grade }
+      it { should validate_presence_of :color }
+      it { should validate_presence_of :height }
+      it { should allow_value(true).for(:top_rope) }
+      it { should allow_value(false).for(:top_rope) }
+      it { should allow_value(true).for(:lead) }
+      it { should allow_value(false).for(:lead) }
     end
   end
 end
