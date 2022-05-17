fb47e813f32d4f41e3e3928c60adcb9493b6c378
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-16 18:47:47 -0600

    feat:US13, Parent Child Creationa

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,11 @@ Rails.application.routes.draw do
   patch '/gyms/:id', to: 'gyms#update'
   get '/gyms/:id/edit', to: 'gyms#edit'
 
+  get '/gyms/:id/rope_routes', to: 'gym_rope_routes#index'
+  post '/gyms/:id/rope_routes', to: 'gym_rope_routes#create'
+  get '/gyms/:id/rope_routes/new', to: 'gym_rope_routes#new'
+
   get '/rope_routes', to: 'rope_routes#index'
   get '/rope_routes/:id', to: 'rope_routes#show'
-  get '/gyms/:gym_id/rope_routes', to: 'gym_rope_routes#index'
 
 end
