3c9b3dc594955cedf19ea3233d8d76c80d6ad344
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-16 18:30:46 -0600

    test: US13, Parent Child Creation

--- spec/features/gyms/rope_routes/index_spec.rb
+++ spec/features/gyms/rope_routes/index_spec.rb
@@ -79,4 +79,50 @@ RSpec.describe "Parent-Child index", type: :feature do
       expect(current_path).to eq("/gyms")
     end
   end
+
+  describe "User Story 12, Parent Child Creation" do
+    # User Story 13, Parent Child Creation
+    #
+    # As a visitor
+    # When I visit a Parent Childs Index page
+    # Then I see a link to add a new adoptable child for that parent "Create Child"
+    # When I click the link
+    # I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child
+    # When I fill in the form with the child's attributes:
+    # And I click the button "Create Child"
+    # Then a `POST` request is sent to '/parents/:parent_id/child_table_name',
+    # a new child object/row is created for that parent,
+    # and I am redirected to the Parent Childs Index page where I can see the new child listed
+    it "has a link to create a new rope route for a gym" do
+      gym_1 = Gym.create!(name: "Movement Englewood", location: "Englewood, CO", has_rope: true, square_feet: 175000)
+
+      visit "/gyms/#{gym_1.id}/rope_routes"
+
+      expect(page).to have_link("Add Rope Route")
+
+      click_link "Add Rope Route"
+
+      expect(current_path).to eq("/gyms/#{gym_1.id}/rope_routes/new")
+    end
+
+    it "can create a new rope route for a gym" do
+      gym_1 = Gym.create!(name: "Movement Englewood", location: "Englewood, CO", has_rope: true, square_feet: 175000)
+
+      visit "/gyms/#{gym_1.id}/rope_routes"
+
+      expect(page).to_not have_content("5.10c")
+
+      visit "/gyms/#{gym_1.id}/rope_routes/new"
+
+      fill_in(:grade, with: "5.10c")
+      fill_in(:color, with: "Black")
+      fill_in(:top_rope, with: true)
+      fill_in(:lead, with: true)
+      fill_in(:height, with: 40)
+      click_button("Add Rope Route")
+
+      expect(current_path).to eq("/gyms/#{gym_1.id}/rope_routes")
+      expect(page).to have_content("5.10c")
+    end
+  end
 end
