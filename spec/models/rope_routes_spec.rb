5f6d35ad1124ac1d84b286d3b001b075b7e6689d
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-17 09:45:50 -0600

    test: #has_top_rope class method

--- spec/models/rope_routes_spec.rb
+++ spec/models/rope_routes_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe RopeRoute, type: :model do
       it { should allow_value(false).for(:lead) }
     end
   end
+
+  describe "class methods" do
+    it "can shows only rope routes with top rope 'true'" do
+      gym_1 = Gym.create!(name: "Movement Englewood", location: "Englewood, CO", has_rope: true, square_feet: 175000)
+      gym_2 = Gym.create!(name: "Movement Boulder", location: "Boulder, CO", has_rope: true, square_feet: 22000)
+      rope_1 = gym_1.rope_routes.create!(grade: '5.9', color: 'Green', top_rope: true, lead: false, height: 33)
+      rope_2 = gym_1.rope_routes.create!(grade: '5.11', color: 'Blue', top_rope: false, lead: true, height: 45)
+      rope_3 = gym_2.rope_routes.create!(grade: '5.10a', color: 'White', top_rope: true, lead: true, height: 33)
+      rope_4 = gym_2.rope_routes.create!(grade: '5.8', color: 'White', top_rope: true, lead: true, height: 38)
+
+      expect(RopeRoute.has_top_rope).to eq([rope_1, rope_3, rope_4])
+    end  
+  end
 end
