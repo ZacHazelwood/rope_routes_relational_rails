98e63dd05292a68365019c0f207e2437c7086dc6
Zachary Hazelwood <zhaze001@gmail.com>
2022-05-17 09:41:34 -0600

    test:US15, 'true' Child records

--- spec/features/rope_routes/index_spec.rb
+++ spec/features/rope_routes/index_spec.rb
@@ -22,4 +22,23 @@ RSpec.describe 'rope_routes#index', type: :feature do
     expect(page).to have_content(rope_1.created_at)
     expect(page).to have_content(rope_1.updated_at)
   end
+
+  describe "User Story 15, Child index shows only 'true' records" do
+    # User Story 15, Child Index only shows `true` Records
+    #
+    # As a visitor
+    # When I visit the child index
+    # Then I only see records where the boolean column is `true`
+    it "only shows rope routes that can be top-roped" do
+      gym_1 = Gym.create!(name: "Movement Englewood", location: "Englewood, CO", has_rope: true, square_feet: 175000)
+      rope_1 = gym_1.rope_routes.create!(grade: '5.9', color: 'Green', top_rope: true, lead: false, height: 33)
+      rope_2 = gym_1.rope_routes.create!(grade: '5.11', color: 'Blue', top_rope: false, lead: true, height: 45)
+
+      visit '/rope_routes'
+
+      save_and_open_page
+      expect(page).to have_content(rope_1.grade)
+      expect(page).to_not have_content(rope_2.grade)
+    end
+  end
 end
